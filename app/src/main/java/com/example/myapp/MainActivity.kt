30d85cf156f1c66c7a578475e2b9505aed639a0e
Elif Vural <elifvural019@gmail.com>
2024-10-08 21:41:00 +0300

    using binding

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,27 +1,41 @@
 package com.example.myapp
 
 import android.os.Bundle
+import android.renderscript.ScriptGroup.Binding
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+        binding.textView.text="hello kotlin!!!"
+
+        /*
     val image= findViewById<ImageView>(R.id.imageView)
         val text= findViewById<TextView>(R.id.textView)
         text.text="hello kotlin!!"
+        */
 
 
 
