4c29fbf95d0f7da80a94ff36a3b7a10e42b2632f
Elif Vural <elifvural019@gmail.com>
2024-10-10 23:22:54 +0300

    toast created with welcome message

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.renderscript.ScriptGroup.Binding
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -48,6 +49,7 @@ class MainActivity : AppCompatActivity() {
         text.text="hello kotlin!!"
         */
 
+        Toast.makeText(this, "welcome!", Toast.LENGTH_SHORT).show()
 
 
     }
@@ -81,6 +83,8 @@ class MainActivity : AppCompatActivity() {
         println("onDestroy executed!")
 
     }
+
+
 
     fun nextPage(view: View){
 
