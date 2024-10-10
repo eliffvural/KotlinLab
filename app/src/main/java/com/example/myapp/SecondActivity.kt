fa09238b252800283b5201ac2c14ba646394fd62
Elif Vural <elifvural019@gmail.com>
2024-10-10 17:59:49 +0300

    second activity created and used with intent

--- app/src/main/java/com/example/myapp/SecondActivity.kt
+++ app/src/main/java/com/example/myapp/SecondActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,9 @@ class SecondActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun previousPage(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
