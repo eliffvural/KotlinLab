0eef172a1bf9c3437ecc9f8069422402bb6d31f5
Elif Vural <elifvural019@gmail.com>
2024-10-10 21:32:01 +0300

    added userLogin to main activity

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -85,6 +85,8 @@ class MainActivity : AppCompatActivity() {
     fun nextPage(view: View){
 
         val intent=Intent(this, SecondActivity::class.java)
+        val userLogin = binding.editText.text.toString()
+        intent.putExtra("name: ", userLogin)
         startActivity(intent)
         //val userLogin= binding.editText.text.toString()
        // binding.textView.text="Name: ${userLogin}"
